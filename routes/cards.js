645b50ae63f9eb84f3ce3533a3d9ca2497cd9822
Maya <Maya@mail.ru>
2023-09-02 05:46:37 +0300

    pr14 - fix4

--- routes/cards.js
+++ routes/cards.js
@@ -12,7 +12,7 @@ router.get('/', cardController.getCards);
 router.post('/', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: Joi.string().uri().required(),
+    link: Joi.string().required().pattern(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/),
   }),
 }), cardController.createCard);
 
